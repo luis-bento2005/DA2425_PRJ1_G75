ea539e192b3b6fd61a7388685ce12aa9aa1cee27
luis-bento2005 <up202305866@up.pt>
2025-03-18 13:45:08 +0000

    Create Graphs done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
+#include "data_structures/createGraphs.h"
+#include "data_structures/Graph.h"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-
+    string folder = "../../DA2425_PRJ1_G75/For_Students";
+    Graph<int> g = createGraphs::graphFromFile(folder);
 
     return 0;
 }
